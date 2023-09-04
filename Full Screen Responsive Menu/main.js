7ac11223a86824e12cf434bcedf5f1ceb4b7f695
Emre <115417234+Emreodesia@users.noreply.github.com>
2023-09-04 22:39:03 +0300

    Full Screen Responsive Menu

---
+++ Full Screen Responsive Menu/main.js
@@ -0,0 +1,11 @@
+    alert("Welcome");
+
+    
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+ 
